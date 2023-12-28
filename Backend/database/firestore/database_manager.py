e6156d1cfcdc9a1f4c19ef9f78c0e1e9a53010a1
Jakub Wojtaś <kubawojtas34@gmail.com>
2023-12-28 11:19:45 +0100

    [ADD] get_by_id enpoint [UPDATE] move DatabaseManager to Singleton

--- Backend/database/firestore/database_manager.py
+++ Backend/database/firestore/database_manager.py
@@ -1,18 +1,31 @@
 import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore_async
-
-cred = credentials.Certificate("./database/firestore/credentials.json")
-
-firebase_admin.initialize_app(cred)
+from firebase_admin import credentials, firestore_async
 
 
 class DatabaseManager:
-    def __init__(self):
-        self.db = firestore_async.client()
+    db = None
+    _instance = None
 
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(DatabaseManager, cls).__new__(cls)
+            cls._instance.db = None
+        return cls._instance
+
+    def init_db(self):
         if self.db is None:
-            raise RuntimeError("Error creating Firestore client")
+            cred = credentials.Certificate("./database/firestore/credentials.json")
+            firebase_admin.initialize_app(cred)
+
+            self.db = firestore_async.client()
+            if self.db is None:
+                raise RuntimeError("Error creating Firestore client")
 
     def get_firestore_client(self):
+        if self.db is None:
+            self.init_db()
         return self.db
+
+
+db_manager = DatabaseManager()
+firestore_client = db_manager.get_firestore_client()
