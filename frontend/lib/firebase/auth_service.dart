d47230c94de78e64bf681f18c81fe9ec6333b01a
pploszczyca <piotrus.plo@gmail.com>
2023-12-29 16:58:50 +0100

    Added singInWithGoogle method

--- frontend/lib/firebase/auth_service.dart
+++ frontend/lib/firebase/auth_service.dart
@@ -1,9 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 import '../domain/result.dart';
 
 class FirebaseAuthenticator {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
 
   Future<Result<UserCredential>> signUp(String email, String password) async {
     try {
@@ -19,6 +21,29 @@ class FirebaseAuthenticator {
   Future<Result<UserCredential>> signIn(String email, String password) async {
     try {
       UserCredential userCredential = await _auth.signInWithEmailAndPassword(email: email, password: password);
+      return Result.success(userCredential);
+    } on FirebaseAuthException catch (e) {
+      return Result.failure(Error(message: "${e.code} ${e.message}"));
+    } catch (e) {
+      return Result.failure(Error(message: e.toString()));
+    }
+  }
+
+  Future<Result<UserCredential>> signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      if (googleUser == null) {
+        return Result.failure(Error(message: "Google sign-in was cancelled by user"));
+      }
+
+      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      UserCredential userCredential = await _auth.signInWithCredential(credential);
       return Result.success(userCredential);
     } on FirebaseAuthException catch (e) {
       return Result.failure(Error(message: "${e.code} ${e.message}"));
