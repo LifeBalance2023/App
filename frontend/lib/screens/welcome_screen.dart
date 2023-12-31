464bf4a1820292465d777980b787897a18336b83
kpusz <kpusz@novomatic-tech.com>
2023-12-31 03:19:57 +0000

    Add font and finish the welcome screen

--- frontend/lib/screens/welcome_screen.dart
+++ frontend/lib/screens/welcome_screen.dart
@@ -37,6 +37,86 @@ class WelcomeScreen extends StatelessWidget {
                       topRight: Radius.circular(55),
                     ),
                   ),
+                  width: double.infinity,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const SizedBox(
+                        height: 20.0,
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(20.0),
+                        child: Text(
+                          'Let’s start to change your balance!',
+                          style: TextStyle(
+                            fontFamily: 'JejuGothic',
+                            fontSize: 24.0,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20.0,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          // Handle login button press
+                        },
+                        style: ButtonStyle(
+                          fixedSize: MaterialStateProperty.all<Size>(
+                              const Size(192, 48)),
+                          backgroundColor: MaterialStateProperty.all<Color?>(
+                              const Color(0xFF4A4E69)),
+                          foregroundColor: MaterialStateProperty.all<Color?>(
+                              const Color(0xFFF2E9E4)),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(
+                                  15.0), // Set the desired corner radius
+                            ),
+                          ),
+                        ),
+                        child: const Text(
+                          'Log in',
+                          style: TextStyle(
+                            fontFamily: 'JejuGothic',
+                            fontSize: 20.0,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 25.0,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          // Handle login button press
+                        },
+                        style: ButtonStyle(
+                          fixedSize: MaterialStateProperty.all<Size>(
+                              const Size(192, 48)),
+                          backgroundColor: MaterialStateProperty.all<Color?>(
+                              const Color(0xFF4A4E69)),
+                          foregroundColor: MaterialStateProperty.all<Color?>(
+                              const Color(0xFFF2E9E4)),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(
+                                  15.0), // Set the desired corner radius
+                            ),
+                          ),
+                        ),
+                        child: const Text(
+                          'Register',
+                          style: TextStyle(
+                            fontFamily: 'JejuGothic',
+                            fontSize: 20.0,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
