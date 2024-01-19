c68fe5f566e3ebf4920af936eb9974ab555ad76b
pploszczyca <piotrus.plo@gmail.com>
2024-01-19 20:36:43 +0100

    Changed AppRouter to use future

--- frontend/lib/router/router.dart
+++ frontend/lib/router/router.dart
@@ -1,38 +1,26 @@
 import 'package:flutter/cupertino.dart';
 
 class AppRouter {
-    static const String main = '/home';
-    static const String welcomeScreen = '/welcomeScreen';
-    static const String login = '/login';
-    static const String register = '/register';
-    static const String taskCreator = '/taskCreator';
-    static const String settings = '/settings';
+  static const String main = '/home';
+  static const String welcomeScreen = '/welcomeScreen';
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String taskCreator = '/taskCreator';
+  static const String settings = '/settings';
 
-    static void goToWelcomeScreen(BuildContext context) {
-        Navigator.pushNamed(context, welcomeScreen);
-    }
-    
-    static void goToLogin(BuildContext context) {
-        Navigator.pushNamed(context, login);
-    }
-    
-    static void goToRegister(BuildContext context) {
-        Navigator.pushNamed(context, register);
-    }
+  static Future<dynamic> goToWelcomeScreen(BuildContext context) => Navigator.pushNamed(context, welcomeScreen);
 
-    static void goToMainScreen(BuildContext context) {
-        Navigator.pushNamed(context, main);
-    }
+  static Future<dynamic> goToLogin(BuildContext context) => Navigator.pushNamed(context, login);
 
-    static void goToTaskCreator(BuildContext context) {
-        Navigator.pushNamed(context, taskCreator);
-    }
+  static Future<dynamic> goToRegister(BuildContext context) => Navigator.pushNamed(context, register);
 
-    static void goToSettings(BuildContext context) {
-        Navigator.pushNamed(context, settings);
-    }
+  static Future<dynamic> goToMainScreen(BuildContext context) => Navigator.pushNamed(context, main);
 
-    static void goBack(BuildContext context) {
-        Navigator.pop(context);
-    }
+  static Future<dynamic> goToTaskCreator(BuildContext context) => Navigator.pushNamed(context, taskCreator);
+
+  static Future<dynamic> goToSettings(BuildContext context) => Navigator.pushNamed(context, settings);
+
+  static void goBack(BuildContext context) {
+    Navigator.pop(context);
+  }
 }
