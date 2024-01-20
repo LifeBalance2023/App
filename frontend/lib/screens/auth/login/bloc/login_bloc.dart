9f24a589e128a1ae47e82ff24c170352f3c5d398
kpusz <kpusz@novomatic-tech.com>
2024-01-20 13:03:34 +0100

    Bloc file to login page added

--- frontend/lib/screens/auth/login/bloc/login_bloc.dart
+++ frontend/lib/screens/auth/login/bloc/login_bloc.dart
@@ -1,29 +1,49 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'login_event.dart';
-import 'login_state.dart';
+import 'package:frontend/screens/auth/login/bloc/login_event.dart';
+import 'package:frontend/screens/auth/login/bloc/login_state.dart';
 
+import '../../../../services/authentication/authentication_service.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
-  LoginBloc() : super(LoginInitial());
+  final AuthenticationService _authenticationService;
 
-  @override
-  Stream<LoginState> mapEventToState(LoginEvent event) async* {
-    if (event is LoginWithGooglePressed) {
-      yield LoginLoading();
-      try {
-        // Perform login with Google
-        yield LoginSuccess();
-      } catch (error) {
-        yield LoginFailure(error: error.toString());
-      }
-    } else if (event is LoginWithCredentialsPressed) {
-      yield LoginLoading();
-      try {
-        // Perform login with credentials
-        yield LoginSuccess();
-      } catch (error) {
-        yield LoginFailure(error: error.toString());
-      }
-    }
+  LoginBloc(this._authenticationService) : super(LoginInitial()) {
+    on<EmailChanged>(_onEmailChanged);
+    on<PasswordChanged>(_onPasswordChanged);
+    on<LoginWithCredentialsRequest>(_onCredentialsLogin);
+    on<LoginWithGoogleRequest>(_onGoogleLogin);
+  }
+
+  void _onEmailChanged(EmailChanged event, Emitter<LoginState> emit) {
+    emit(LoginEdited(email: event.email, password: state.password));
+  }
+
+  void _onPasswordChanged(PasswordChanged event, Emitter<LoginState> emit) {
+    emit(LoginEdited(email: state.email, password: event.password));
+  }
+
+  Future<void> _onCredentialsLogin(
+      LoginWithCredentialsRequest event, Emitter<LoginState> emit) async {
+    emit(LoginLoading(email: state.email, password: state.password));
+    final result = await _authenticationService.signIn(
+        email: state.email, password: state.password);
+
+    result
+        .onFailure((error) => emit(LoginFailure(
+            email: state.email, password: state.password, error: error)))
+        .onSuccess((_) =>
+            emit(LoginSuccess(email: state.email, password: state.password)));
+  }
+
+  Future<void> _onGoogleLogin(
+      LoginWithGoogleRequest event, Emitter<LoginState> emit) async {
+    emit(LoginLoading(email: state.email, password: state.password));
+    final result = await _authenticationService.signInWithGoogle();
+
+    result
+        .onFailure((error) => emit(LoginFailure(
+            email: state.email, password: state.password, error: error)))
+        .onSuccess((_) =>
+            emit(LoginSuccess(email: state.email, password: state.password)));
   }
 }
