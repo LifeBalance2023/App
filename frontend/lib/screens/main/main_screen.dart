b1a1025fbb30f5cec3dfc08fa883b9cb24ec368f
kpusz <kpusz@novomatic-tech.com>
2024-01-21 21:33:23 +0100

    Add main screen drawer menu

--- frontend/lib/screens/main/main_screen.dart
+++ frontend/lib/screens/main/main_screen.dart
@@ -16,7 +16,58 @@ class MainScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Main Screen'),
+        title: const Text(
+          'Main screen',
+          style: TextStyle(
+            fontFamily: 'JejuGothic',
+            fontSize: 20.0,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+                decoration: const BoxDecoration(
+                  color: Color(0xFF9A8C98),
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 100,
+                      width: 100,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Text('@user_id'),
+                  ],
+                )),
+            ListTile(
+              title: const Text('Settings'),
+              trailing: const Icon(Icons.settings_sharp),
+              onTap: () {
+                Navigator.pop(context);
+                AppRouter.goToSettings(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Sign out'),
+              trailing: const Icon(Icons.power_settings_new_sharp),
+              onTap: () {
+                Navigator.pop(context);
+                mainScreenBloc.add(SignOutRequest());
+              },
+            )
+          ],
+        ),
       ),
       body: BlocConsumer<MainScreenBloc, MainScreenState>(
         listener: (context, state) {
@@ -44,19 +95,8 @@ class MainScreen extends StatelessWidget {
                   'Main Screen ${state.tasks.length} ${state.statistics.amountOfAllTasks}'),
             );
           }
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text('Error'),
-                ElevatedButton(
-                  onPressed: () {
-                    mainScreenBloc.add(SignOutRequest());
-                  },
-                  child: const Text('Sign out'),
-                )
-              ],
-            ),
+          return const Center(
+            child: Text('Error'),
           );
         },
       ),
