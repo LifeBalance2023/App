08a41127eb46e060d031bc71b7652e5e24541636
Conrad-Push <konrad.pusz7@gmail.com>
2024-01-24 00:54:00 +0100

    Add signOut action into main screen bloc and do some styling for Task adding floating button

--- frontend/lib/screens/main/bloc/main_screen_bloc.dart
+++ frontend/lib/screens/main/bloc/main_screen_bloc.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:frontend/screens/main/bloc/main_screen_event.dart';
 import 'package:frontend/screens/main/bloc/main_screen_state.dart';
@@ -12,12 +11,16 @@ class MainScreenBloc extends Bloc<MainScreenEvent, MainScreenState> {
   final TasksService _tasksService;
   final StatisticsService _statisticsService;
 
-  MainScreenBloc(this._authenticationService, this._tasksService, this._statisticsService) : super(ShowProgressIndicator()) {
+  MainScreenBloc(
+      this._authenticationService, this._tasksService, this._statisticsService)
+      : super(ShowProgressIndicator()) {
     on<LoadMainScreen>(_onLoadMainScreen);
     on<GetTasksAndStatistics>(_onGetTasksAndStatistics);
+    on<SignOutRequest>(_onSignOutRequest);
   }
 
-  Future<void> _onLoadMainScreen(LoadMainScreen event, Emitter<MainScreenState> emit) async {
+  Future<void> _onLoadMainScreen(
+      LoadMainScreen event, Emitter<MainScreenState> emit) async {
     final userIdResult = await _authenticationService.getUserId();
 
     if (userIdResult.isSuccess) {
@@ -31,7 +34,8 @@ class MainScreenBloc extends Bloc<MainScreenEvent, MainScreenState> {
     final synchronizeTasksResult = await _tasksService.synchronizeTasks();
 
     if (synchronizeTasksResult.isFailure) {
-      emit(MainScreenError("Error synchronizing tasks: ${synchronizeTasksResult.error?.code} ${synchronizeTasksResult.error?.message}}"));
+      emit(MainScreenError(
+          "Error synchronizing tasks: ${synchronizeTasksResult.error.code} ${synchronizeTasksResult.error.message}}"));
       return;
     }
 
@@ -39,21 +43,36 @@ class MainScreenBloc extends Bloc<MainScreenEvent, MainScreenState> {
   }
 
   Future<void> _loadTasksAndStatistics(Emitter<MainScreenState> emit) async {
-    final getStatisticsResult = await _statisticsService.getDailyStatistics(DateTime.now());
+    final getStatisticsResult =
+        await _statisticsService.getDailyStatistics(DateTime.now());
     getStatisticsResult.onFailure((error) {
-      emit(MainScreenError("Error getting statistics: ${error.code} ${error.message}}"));
+      emit(MainScreenError(
+          "Error getting statistics: ${error.code} ${error.message}}"));
     }).onSuccess((statistics) {
-      final tasksResult = _tasksService.getAllTasks();    // TODO: Choose specific date
+      final tasksResult =
+          _tasksService.getAllTasks(); // TODO: Choose specific date
 
       tasksResult.onFailure((error) {
-        emit(MainScreenError("Error getting tasks: ${error.code} ${error.message}}"));
+        emit(MainScreenError(
+            "Error getting tasks: ${error.code} ${error.message}}"));
       }).onSuccess((tasks) {
         emit(ShowMainScreen(tasks, statistics));
       });
     });
   }
 
-  Future<void> _onGetTasksAndStatistics(GetTasksAndStatistics event, Emitter<MainScreenState> emit) async {
+  Future<void> _onGetTasksAndStatistics(
+      GetTasksAndStatistics event, Emitter<MainScreenState> emit) async {
     await _loadTasksAndStatistics(emit);
+  }
+
+  Future<void> _onSignOutRequest(
+      SignOutRequest event, Emitter<MainScreenState> emit) async {
+    final signOutResult = await _authenticationService.signOut();
+
+    signOutResult
+        .onFailure((error) => emit(MainScreenError(
+            "Error while signing out: ${error.code} ${error.message}}")))
+        .onSuccess((_) => emit(GoToWelcomeScreen()));
   }
 }
--- frontend/lib/screens/main/main_screen.dart
+++ frontend/lib/screens/main/main_screen.dart
@@ -16,45 +16,123 @@ class MainScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Main Screen'),
+        title: const Text(
+          'Main screen',
+          style: TextStyle(
+            fontFamily: 'JejuGothic',
+            fontSize: 20.0,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+                decoration: const BoxDecoration(
+                  color: Color(0xFF9A8C98),
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 100,
+                      width: 100,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Text('@user_id'),
+                  ],
+                )),
+            ListTile(
+              title: const Text('Settings'),
+              trailing: const Icon(Icons.settings_sharp),
+              onTap: () {
+                Navigator.pop(context);
+                AppRouter.goToSettings(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Sign out'),
+              trailing: const Icon(Icons.power_settings_new_sharp),
+              onTap: () {
+                Navigator.pop(context);
+                mainScreenBloc.add(SignOutRequest());
+              },
+            )
+          ],
+        ),
       ),
       body: BlocConsumer<MainScreenBloc, MainScreenState>(
         listener: (context, state) {
-          if (state is MainScreenError) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text(state.errorMessage),
-              ),
-            );
-          }
-
-          if (state is GoToWelcomeScreen) {
-            AppRouter.goToWelcomeScreen(context);
-          }
+          _blocListener(state, context);
         },
         builder: (context, state) {
-          if(state is ShowProgressIndicator) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          if (state is ShowMainScreen) {
-            return Center(
-              child: Text('Main Screen ${state.tasks.length} ${state.statistics.amountOfAllTasks}'),
-            );
-          }
-          return const Center(
-            child: Text('Error'),
-          );
+          return _blocBuilder(state, context);
         },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          await AppRouter.goToTaskCreator(context);
-          mainScreenBloc.add(GetTasksAndStatistics());
-        },
-        child: const Icon(Icons.add),
-      )
+      floatingActionButton: SizedBox(
+        height: 70,
+        width: 70,
+        child: FittedBox(
+          child: FloatingActionButton(
+            onPressed: () async {
+              await AppRouter.goToTaskCreator(context);
+              mainScreenBloc.add(GetTasksAndStatistics());
+            },
+            shape: const CircleBorder(),
+            backgroundColor: const Color(0xFF81767F),
+            foregroundColor: Colors.black,
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.add,
+                  size: 15,
+                ),
+                Text('Task'),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
+}
+
+void _blocListener(MainScreenState state, BuildContext context) {
+  if (state is MainScreenError) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(state.errorMessage),
+      ),
+    );
+  }
+
+  if (state is GoToWelcomeScreen) {
+    AppRouter.goToWelcomeScreen(context);
+  }
+}
+
+Widget _blocBuilder(MainScreenState state, BuildContext context) {
+  if (state is ShowProgressIndicator) {
+    return const Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+  if (state is ShowMainScreen) {
+    return Center(
+      child: Text(
+          'Main Screen ${state.tasks.length} ${state.statistics.amountOfAllTasks}'),
+    );
+  }
+  return const Center(
+    child: Text('Error'),
+  );
 }
