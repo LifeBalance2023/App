5f585b15172e46e12fa7e569d62a376efa502cd8
pploszczyca <piotrus.plo@gmail.com>
2024-01-25 12:33:26 +0100

    Increased progress indicator height

--- frontend/lib/components/custom_progress_indicator.dart
+++ frontend/lib/components/custom_progress_indicator.dart
@@ -6,9 +6,10 @@ class CustomProgressIndicator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Center(
+        heightFactor: 2,
         child: CircularProgressIndicator(
       color: Color(0xFF81767F),
-      strokeWidth: 6.0,
+      strokeWidth: 5.0,
     ));
   }
 }
